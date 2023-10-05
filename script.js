d7154bbc86895ec8a83ebd11c5c3b13a593295e1
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-10-05 18:47:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 //your code here!
+// Create initial list items
+for(let i = 0; i < 10; i++) {
+    let listItem = document.createElement('li');
+    listItem.textContent = `Item ${i+1}`;
+    document.querySelector('#infi-list').appendChild(listItem);
+}
+
+// Function to add new list items
+function addItems() {
+    let currentItems = document.querySelectorAll('li').length;
+    for(let i = currentItems; i < currentItems + 2; i++) {
+        let listItem = document.createElement('li');
+        listItem.textContent = `Item ${i+1}`;
+        document.querySelector('#infi-list').appendChild(listItem);
+    }
+}
+
+let listElement = document.querySelector('#infi-list');
+
+listElement.addEventListener('scroll', function() {
+    let d = listElement;
+    let offset = d.scrollTop + d.clientHeight;
+    let height = d.scrollHeight;
+
+    if (offset >= height) {
+        addItems();
+    }
+});
 
